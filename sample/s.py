184996d14f6406fb7de574b59a4f35e6129a9f9f
saeed <saeedmr881@gmail.com>
2023-09-28 18:02:02 +0330

    finish mask voxel

--- sample/s.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import open3d as o3d
-
-
-scan = np.fromfile("/home/saeed/Downloads/MDImagePyramidReference.unknown.0000002.dat", dtype=np.float32)
-
-print(scan[0])
